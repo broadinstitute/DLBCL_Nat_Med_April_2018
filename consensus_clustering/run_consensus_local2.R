083fea53728ac2d42c059a04d29c250f4905d8c4
adunford <adunford@cga-kras.broadinstitute.org>
2018-03-15 17:21:29 -0400

    adding input data necessary to run consensus clustering

--- consensus_clustering/run_consensus_local2.R
+++ consensus_clustering/run_consensus_local2.R
@@ -1,2 +1,2 @@
-source("/xchip/cga_home/adunford/DLBCL/Consensus_Clustering/local_run/GDAC_selectBestcluster/select_best_cluster_chc.R")
-result <- main("-uDLBCL_no_21q22.3.expclu.gct","-mPearson","-cDLBCL_no_21q22.3.cophenetic.coefficient.txt","-wDLBCL_no_21q22.3.membership.txt","-vDLBCL_no_21q22.3","-p/xchip/cga_home/adunford/DLBCL/Gene_Sample_Matrix/DLBCL_mutation_scna_sv_matrix.no_21q22.3.21_July_2017.txt")
+source("src/GDAC_selectBestcluster/select_best_cluster_chc.R")
+result <- main("-uoutput/DLBCL.expclu.gct","-mPearson","-coutput/DLBCL.cophenetic.coefficient.txt","-woutput/DLBCL.membership.txt","-voutput/DLBCL","-pinput_data/DLBCL_significant_event_matrix.txt")
