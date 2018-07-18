a444b386fda15e40ecee35bfe13f6f1fa969f169
adunford <adunford@wpe4c-cdf.broad.mit.edu>
2018-07-18 12:16:12 -0400

    attempting to edit run_consensus_clustering to preserve intermediate files

--- consensus_clustering/run_consensus_clustering.R
+++ consensus_clustering/run_consensus_clustering.R
@@ -1,8 +1,8 @@
 source('src/GDAC_TopgenesforCluster/Topgenes_v1.R')
-result <- main("-s./src/GDAC_TopgenesforCluster/","-minput_data/DLBCL_significant_event_matrix.txt","-uALL","-oDLBCL")
+result <- main("-s./src/GDAC_TopgenesforCluster/","-minput_data/DLBCL_significant_event_matrix.txt","-uALL","-ooutput_dir/DLBCL")
 
 
 source("src/GDAC_NmfConsensusClustering/GDAC_CNMF.R", echo = TRUE)
-result <- main("-ssrc/GDAC_NmfConsensusClustering/","-mDLBCL.expclu.gct","-oDLBCL",'-u4','-v10') 
+result <- main("-ssrc/GDAC_NmfConsensusClustering/","-moutput_dir/DLBCL.expclu.gct","-ooutput_dir/DLBCL",'-u4','-v10') 
 source("src/GDAC_selectBestcluster/select_best_cluster_chc.R")
-result <- main("-uDLBCL.expclu.gct","-mPearson","-cDLBCL.cophenetic.coefficient.txt","-wDLBCL.membership.txt","-voutput_dir/DLBCL","-pinput_data/DLBCL_significant_event_matrix.txt")
+result <- main("-uoutput_dir/DLBCL.expclu.gct","-mPearson","-coutput_dir/DLBCL.cophenetic.coefficient.txt","-woutput_dir/DLBCL.membership.txt","-voutput_dir/DLBCL","-pinput_data/DLBCL_significant_event_matrix.txt")
